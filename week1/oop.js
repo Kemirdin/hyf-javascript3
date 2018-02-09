90cafd2d22bb02a209bb19f6bb5a257e66721e6b
Kemirdin <qemirdin@gmail.com>
2018-02-09 21:44:34 +0100

    update file

--- week1/oop.js
+++ week1/oop.js
@@ -150,6 +150,6 @@ space ();
 
 
 
-console.log(PrisonBreak.getStars().map(actor => `${actor.getName()} ${actor.getAge()}`));
-const director = PrisonBreak.getDirector();
+console.log(Movie.getStars().map(actor => `${actor.getName()} ${actor.getAge()}`));
+const director = Movie.getDirector();
 console.log(`Director: ${director.map(director => `${director.getName()}`)}`);
