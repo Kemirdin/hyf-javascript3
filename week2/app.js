ef2721c3ee52b51d09c09a5093325b48ab9e6acb
Kemirdin <qemirdin@gmail.com>
2018-02-15 21:00:41 +0100

    update file

--- week2/app.js
+++ week2/app.js
@@ -16,47 +16,7 @@ input.addEventListener ('keydown', function () {
   if (event.keyCode == 13) loading ();
 });
 
-// function checkTheName () {
-//   // this function runs when pressing
-//   detailsDiv.innerHTML = ''; // the search button, and it makes http requests
-//   let userName = search.value;
-//   detailsDiv.appendChild (h2);
-//   detailsDiv.appendChild (h3);
-//   makeRequest (
-//     'https://api.github.com/users/' + userName + '/repos'
-//   ).then (data => {
-//     img.src = data[0].owner.avatar_url;
-//     h2.innerHTML = userName;
-//     detailsDiv.appendChild (img);
-//     let repoName = '';
-//     data.forEach (repo => {
-//       repoName = repo.name;
-//       let li = document.createElement ('li');
-//       li.innerHTML = repoName;
-//       h3.innerHTML = userName + "'s repositories:";
-//       let link = repo.html_url;
-//       let a = document.createElement ('a');
-//       a.href = link;
-//       makeRequest (
-//         'https://api.github.com/repos/' + userName + '/' + repoName + '/commits'
-//       ).then (commit => {
-//         let lastCommit = commit[0];
-//         let authorImg = document.createElement ('img');
-//         authorImg.src = lastCommit.author.avatar_url;
-//         authorImg.className = 'author-img';
-//         h3.appendChild (a);
-//         let p = document.createElement ('p');
-//         let commitDate = lastCommit.commit.author.date;
-//         let commitAuthor = lastCommit.commit.author.name;
-//         p.innerHTML =
-//           'Last commit was at ' + commitDate + ' by ' + commitAuthor;
-//         li.appendChild (p);
-//         li.appendChild (authorImg);
-//         a.appendChild (li);
-//       });
-//     });
-//   });
-// }
+
 
 // Create the XHR object.
 function createCORSRequest (method, url) {
