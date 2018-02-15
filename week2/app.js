7f8c5411dd50810c81a6b19d6fd218104042f29c
Kemirdin <qemirdin@gmail.com>
2018-02-15 18:32:20 +0100

    update file

--- week2/app.js
+++ week2/app.js
@@ -1,6 +1,17 @@
-const http = new XMLHttpRequest ();
+//create request with its porperties
+var request = new httpRequest ();
+request.method = 'GET';
+request.url = 'https://api.github.com/orgs/HackYourFuture/repos';
 
-http.open ('GET', 'https://api.github.com/orgs/HackYourFuture/repos');
-http.send ();
+//create callback for success containing the response
+request.success = function (response) {
+  console.log (response);
+};
 
-http.onload = () => console.log (http.responseText);
+//and a fail callback containing the error
+request.fail = function (error) {
+  console.log (error);
+};
+
+//and finally send it away
+request.send ();
