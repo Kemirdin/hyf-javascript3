f10e8a2e01af4689a219563cfaa6bbaef6b0959c
Kemirdin <qemirdin@gmail.com>
2018-02-16 18:00:40 +0100

    update file

--- week2/app.js
+++ week2/app.js
@@ -74,42 +74,42 @@ function renderHTML(data, amount) {
 
 
 
-  function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest ();
-  if ('withCredentials' in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open (method, url, true);
-  } else if (typeof XDomainRequest != 'undefined') {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest ();
-    xhr.open (method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
+//   function createCORSRequest(method, url) {
+//   var xhr = new XMLHttpRequest ();
+//   if ('withCredentials' in xhr) {
+//     // XHR for Chrome/Firefox/Opera/Safari.
+//     xhr.open (method, url, true);
+//   } else if (typeof XDomainRequest != 'undefined') {
+//     // XDomainRequest for IE.
+//     xhr = new XDomainRequest ();
+//     xhr.open (method, url);
+//   } else {
+//     // CORS not supported.
+//     xhr = null;
+//   }
+//   return xhr;
+// }
 
-// Make the actual CORS request.
-function makeCorsRequest () {
-  // This is a sample server that supports CORS.
-  var url = 'https://api.github.com/orgs/HackYourFuture/repos';
+// // Make the actual CORS request.
+// function makeCorsRequest () {
+//   // This is a sample server that supports CORS.
+//   var url = 'https://api.github.com/orgs/HackYourFuture/repos';
 
-  var xhr = createCORSRequest ('GET', url);
-  if (!xhr) {
-    alert ('CORS not supported');
-    return;
-  }
+//   var xhr = createCORSRequest ('GET', url);
+//   if (!xhr) {
+//     alert ('CORS not supported');
+//     return;
+//   }
 
-  // Response handlers.
-  xhr.onload = function () {
-    var text = xhr.responseText;
-    alert ('Response from CORS request to ' + url + ': ' + text);
-  };
+//   // Response handlers.
+//   xhr.onload = function () {
+//     var text = xhr.responseText;
+//     alert ('Response from CORS request to ' + url + ': ' + text);
+//   };
 
-  xhr.onerror = function () {
-    alert ();
-  };
+//   xhr.onerror = function () {
+//     alert ();
+//   };
 
-  xhr.send ();
-}
+//   xhr.send ();
+// }
