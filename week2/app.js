96043007fcc24affeff50539da27ea1b86586562
Kemirdin <qemirdin@gmail.com>
2018-02-16 20:53:26 +0100

    update file

--- week2/app.js
+++ week2/app.js
@@ -1,23 +1,23 @@
 "use strict";
-var body = window.document.getElementsByTagName("body")[0];
-var resultEl = document.querySelector("#resultEl");
-var loaderEl = document.getElementById("resultLoader");
-var search = document.getElementById('search');
-var button = document.getElementById ('button');
- var input = document.getElementById('input');
+let body = window.document.getElementsByTagName("body");
+let resultEl = document.querySelector("#resultEl");
+let loaderEl = document.getElementById("resultLoader");
+let search = document.getElementById('search');
+let button = document.getElementById('button');
+let input = document.getElementById('input');
 
-function loading () {
-  search.classList.add ('loading');
+function loading() {
+  search.classList.add('loading');
 
-  setTimeout (function () {
-    search.classList.remove ('loading');
+  setTimeout(function () {
+    search.classList.remove('loading');
   }, 1500);
 }
 
-button.addEventListener ('click', loading);
+button.addEventListener('click', loading);
 
-input.addEventListener ('keydown', function () {
-  if (event.keyCode == 13) loading ();
+input.addEventListener('keydown', function () {
+  if (event.keyCode == 13) loading();
 });
 
 
@@ -25,7 +25,7 @@ input.addEventListener ('keydown', function () {
 // Create the XHR object.
 
 (function () {
-  var httpRequest;
+  let httpRequest;
   document.getElementById("button").addEventListener('click', makeRequest);
 
   function makeRequest() {
