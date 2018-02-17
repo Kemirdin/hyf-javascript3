6ae904cc902c14eb3b372755933fac7272faf9a1
Kemirdin <qemirdin@gmail.com>
2018-02-17 19:01:51 +0100

    update file

--- week2/modified.js
+++ week2/modified.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//Search button  
 const search = document.getElementById('search');
 const button = document.getElementById ('button');
 const input = document.getElementById ('input');
@@ -18,3 +18,26 @@ button.addEventListener ('click', loading);
 input.addEventListener ('keydown', function () {
   if (event.keyCode == 13) loading ();
 });
+
+// Create the XHR object.
+
+(function () {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=London,England';
+  var apiKey = 'APIKEY'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+  var httpRequest;
+  makeRequest ();
+
+  // create and send an XHR request
+  function makeRequest () {
+    httpRequest = new XMLHttpRequest ();
+    httpRequest.onreadystatechange = responseMethod;
+    httpRequest.open ('GET', url + '&appid=' + apiKey);
+    httpRequest.send ();
+  }
+  // handle XHR response
+  function responseMethod () {
+    if (httpRequest.readyState === 4) {
+      console.log (httpRequest.responseText);
+    }
+  }
+}) ();
