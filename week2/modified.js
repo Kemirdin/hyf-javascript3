4cac14e389d23b365b035409a7e32ba2f569086a
Kemirdin <qemirdin@gmail.com>
2018-02-17 18:59:25 +0100

    update file

---
+++ week2/modified.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+const search = document.getElementById('search');
+const button = document.getElementById ('button');
+const input = document.getElementById ('input');
+
+function loading () {
+  search.classList.add ('loading');
+
+  setTimeout (function () {
+    search.classList.remove ('loading');
+  }, 1500);
+}
+
+button.addEventListener ('click', loading);
+
+input.addEventListener ('keydown', function () {
+  if (event.keyCode == 13) loading ();
+});
