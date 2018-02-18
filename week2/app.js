42dba050f7d897f404e822a8a0a2fc92dec0c384
Kemirdin <qemirdin@gmail.com>
2018-02-18 12:09:25 +0100

    update file

--- week2/app.js
+++ week2/app.js
@@ -2,75 +2,35 @@
 
   //Search button
   const button = document.getElementById('button');
-  
+const container = document.getElementById("getResults");
   const search = document.getElementById ('search');
-  search.addEventListener('click', myXML, true);
-  function myXML() {
-  const input = document.getElementById ('input').value;
-
-
-  const url = 'https://api.github.com/orgs/HackYourFuture/repos';
-  const httpRequest;
-
-  document.getElementById("button").addEventListener('click', makeRequest);
-
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
-      return false;
-    }
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', url);
-    const myData = JSON.parse (httRequest.responseText);
-renderHTML (myData);
-
-    httpRequest.send();
-  }
-
-  function alertContents() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        alert(httpRequest.responseText);
-      } else {
-        alert('There was a problem with the request.');
-      }
-    }
-  }
-}) ();
-
-//actually, I would like to get 'repo info' but still has few problems
-//my renderHtmle functions doesn't  work
-function renderHTML (myData;
-) {
-  var htmlString = '';
-
-  for (i = 0; i < myData.length; i++) {
-    htmlString +=
-      '<p>' + myData[i].name + ' is a ' + myData[i].name + ' Repo ';
-    for (ii = 0; ii < myData[i].id.name.length; ii++) {
-      if (ii == 0) {
-        htmlString += myData[i].foods.id[ii];
-      } else {
-        htmlString += ' and ' + myData[i].id.name[ii];
-      }
-    }
-    htmlString += ' and  ';
-    for (ii = 0; ii < myData[i].id.name.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].id.name[ii];
-      } else {
-        htmlString += ' and ' + data[i].id.name[ii];
-      }
-    }
-    htmlString += '.</p>';
-  }
-  myContainer.insertAdjacentHTML ('beforeend', htmlString);
-}
-
-
+  search.addEventListener('click', makeRequest);
   
+const input = document.getElementById('input');
+
+
+const url = 'https://api.github.com/repos/HackYourFuture/';
+
+document.getElementById("button").addEventListener('click', makeRequest);
+
+function makeRequest() {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.open("GET", url + input.value);
+  httpRequest.responseType = "json";
+  httpRequest.send();
+  httpRequest.onload = function () {
+    const data = httpRequest.response;
+    renderRepositories(data);
+  };
+  }
+  
+  function renderRepositories(val) {
+    const h1 = createAndAppend("h1", container, val.name);
+    const a = createAndAppend("a", container, val.html_url);
+    a.setAttribute("href", val.html_url);
+    a.setAttribute("target", "_blank");
+
+}     
   // create and append function
   
 function createAndAppend (name, append, innerHTML) {
@@ -81,45 +41,3 @@ function createAndAppend (name, append, innerHTML) {
   }
   return child;
 }
-
-  // Fetch request
-  //actually, I would like to make Fetch but still has few problems
-//my Fetch functions doesn't  work
-
-
-  (function() {
-	var url = "https://api.github.com/orgs/HackYourFuture/repos";
-	
-
-	fetch(url).then(function(response) {
-		if (!response.ok) {
-			throw Error(response.statusText);
-		}
-		return(response.json());
-	}).then(function(response) {
-		renderHTML(response);
-	}).catch(function() {
-		updateUIError();
-	});
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
-
