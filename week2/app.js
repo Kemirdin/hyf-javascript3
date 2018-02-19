45dfb32eead3a98dde2864fbfc02530099da8942
Kemirdin <qemirdin@gmail.com>
2018-02-19 18:55:14 +0100

    update file

--- week2/app.js
+++ week2/app.js
@@ -30,47 +30,48 @@ function makeRequest() {
   }
   // render repository button click
 
-  function renderRepositories(data) {
-    const h1 = createAndAppend("h1", getRsults, data.full_name);
-    const ul = createAndAppend("ul", getRsults);
-    const li = createAndAppend("li", ul);
-    const a = createAndAppend("a", li, data.html_url);
-    a.setAttribute("href", data.html_url);
-    a.setAttribute("target", "_blank");
-    const xhrCont = new XMLHttpRequest();
-    xhrCont.open("GET", data.contributors_url);
-    xhrCont.send();
-    xhrCont.responseType = "json";
-    xhrCont.onload = function () {
-        const output = xhrCont.response;
-        getContributors(output);
+function renderRepositories(data) {
+  const h1 = createAndAppend("h1", getRsults, data.full_name);
+  const ul = createAndAppend("ul", getRsults);
+  const li = createAndAppend("li", ul);
+  const a = createAndAppend("a", li, data.html_url);
+  a.setAttribute("href", data.html_url);
+  a.setAttribute("target", "_blank");
+  const xhrCont = new XMLHttpRequest();
+  xhrCont.open("GET", data.contributors_url);
+  xhrCont.send();
+  xhrCont.responseType = "json";
+  xhrCont.onload = function () {
+    const output = xhrCont.response;
+    getContributors(output);
 
-    }     
-//get contributer date function
-function getContributors (data) {
-  for (let i = 0; i < data.length; i++) {
-    const ul = createAndAppend ('ul', getRsults);
-    const li = createAndAppend ('li', ul);
-    const a = createAndAppend (
-      'a',
-      li,
-      '<hr><br>' + data[i].login + '<br><br>'
-    );
-    a.setAttribute ('href', data[i].html_url);
-    a.setAttribute ('target', '_blank');
-    const img = createAndAppend ('img', li);
-    img.setAttribute ('src', data[i].avatar_url);
   }
-}
+  //get contributer date function
+  function getContributors(data) {
+    for (let i = 0; i < data.length; i++) {
+      const ul = createAndAppend('ul', getRsults);
+      const li = createAndAppend('li', ul);
+      const a = createAndAppend(
+        'a',
+        li,
+        '<hr><br>' + data[i].login + '<br><br>'
+      );
+      a.setAttribute('href', data[i].html_url);
+      a.setAttribute('target', '_blank');
+      const img = createAndAppend('img', li);
+      img.setAttribute('src', data[i].avatar_url);
+    }
+  }
     
   // create and append function
     
   
-function createAndAppend (name, append, innerHTML) {
-  const child = document.createElement (name);
-  append.appendChild (child);
-  if (innerHTML !== undefined) {
-    child.innerHTML = innerHTML;
+  function createAndAppend(name, append, innerHTML) {
+    const child = document.createElement(name);
+    append.appendChild(child);
+    if (innerHTML !== undefined) {
+      child.innerHTML = innerHTML;
+    }
+    return child;
   }
-  return child;
 }
