0746a89ae3fe40c3a0f41c2ef9b4c1571158f27d
Kemirdin <qemirdin@gmail.com>
2018-02-19 21:12:45 +0100

    update file

--- week2/Modified/modified.js
+++ week2/Modified/modified.js
@@ -1,53 +1,42 @@
-'use strict';
+function makeRequest (url, methodType, callback) {
+  const xhr = new XMLHttpRequest ();
+  xhr.open (methodType, url, true);
+  xhr.send ();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log ('xhr done successfully');
+        var resp = xhr.responseText;
+        var respJson = JSON.parse (resp);
+        callback (respJson);
+      } else {
+        console.log ('xhr failed');
+      }
+    } else {
+      console.log ('xhr processing going on');
+    }
+  };
+  console.log ('request sent succesfully');
+}
 
-//Search button  
-const search = document.getElementById('search');
-const button = document.getElementById ('button');
-const input = document.getElementById ('input');
-
-$ ('button').on ('click', function () {
-  $ ('.repo-list').append ('<p class="loading">Loading...</p>');
-
-  $.ajax ({
-    method: 'GET',
-    url: 'https://api.github.com/repos/HackYourFuture/CommandLine',
-  })
-    .done (function (data) {
-      $.each (data, function (index, value) {
-        console.log (value);
-        $ ('ul').append ('<li>' + index + ':' + value + '</li>');
-        //other items go in here.
-      });
-    })
-    .fail (function () {
-      console.log ('fail');
-    })
-    .always (function () {
-      $ ('button').css ('color', 'red');
-      $ ('.loading').remove ();
-    });
+document.getElementById ('userDetails').addEventListener ('click', function () {
+  // git hub url to get btford details
+  var userId = document.getElementById ('userId').value;
+  var URL = 'https://api.github.com/users/' + userId;
+  makeAjaxCall (URL, 'GET', processUserDetailsResponse);
 });
-  
-$ ('button').on ('click', function () {
-  // Insert <p> to appear while waiting for list to append
-  // $('<p class="pending-message">Pending...!</p>').insertBefore('repo-list');
-  $ ('.repo-list').before ('<p class="pending-message">Loading...</p>');
 
-  $.getJSON (
-    'https://api.github.com/repos/HackYourFuture/CommandLine'
-  )
-    .done (function (data) {
-      $.each (data, function (index, value) {
-        // Need to find out what the property name is first, can use console.log()
-        // console.log(data);
-        $ ('.repo-list').append ('<li>' + value.name + '</li>');
-      }); // Chain on error handling with .fail() to ensure action runs
-    })
-    .fail (function () {
-      $ ('.repo-list').append ('<li>' + "Sorry, it's not working!" + '</li>');
-    })
-    .always (function () {
-      // Removes pending message above
-      $ ('.pending-message').remove ();
-    });
+document.getElementById ('repoList').addEventListener ('click', function () {
+  // git hub url to get btford details
+  var userId = document.getElementById ('userId').value;
+  var URL = 'https://api.github.com/users/' + userId + '/repos';
+  makeRequest (URL, 'GET', processRepoListResponse);
 });
+
+function processUserDetailsResponse (userData) {
+  console.log ('render user details', userData);
+}
+
+function processRepoListResponse (repoList) {
+  console.log ('render repo list', repoList);
+}
