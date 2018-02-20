aeb689cfd8b58e315ba1d99ddb2b98ac357b0499
Kemirdin <qemirdin@gmail.com>
2018-02-20 23:35:06 +0100

    update file

--- week2/modified.js
+++ week2/modified.js
@@ -1,48 +1,44 @@
 'use strict';
 
-  function makeRequest (url, methodType, callback) {
-    const xhr = new XMLHttpRequest ();
-    xhr.open (methodType, url, true);
-    xhr.send ();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          console.log ('xhr done successfully');
-          const resp = xhr.responseText;
-          const respJson = JSON.parse (resp);
-          callback (respJson);
-        } else {
-          console.log ('xhr failed');
-        }
+function makeRequest (url, methodType, callback) {
+  const xhr = new XMLHttpRequest ();
+  xhr.open (methodType, url, true);
+  xhr.send ();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log ('xhr done successfully');
+        const resp = xhr.responseText;
+        const respJson = JSON.parse (resp);
+        callback (respJson);
       } else {
-        console.log ('xhr processing going on');
+        console.log ('xhr failed');
       }
-    };
-    console.log ('request sent succesfully');
-  }
+    } else {
+      console.log ('xhr processing going on');
+    }
+  };
+  console.log ('request sent succesfully');
+}
 
-  document
-    .getElementById ('userDetails')
-    .addEventListener ('click', function () {
-      // git hub url to get btford details
-      const userId = document.getElementById ('userId').value;
-      const URL = 'https://api.github.com/users/' + userId;
-      makeRequest (URL, 'GET', processUserDetailsResponse);
-    });
+document.getElementById ('userDetails').addEventListener ('click', function () {
+  // git hub url to get btford details
+  const userId = document.getElementById ('userId').value;
+  const URL = 'https://api.github.com/users/' + userId;
+  makeRequest (URL, 'GET', userDetailsResponse);
+});
 
-  document.getElementById ('repoList').addEventListener ('click', function () {
-    // git hub url to get btford details
-    const userId = document.getElementById ('userId').value;
-    const URL =
-      'https://api.github.com/users/' + userId + '/repos';
-    makeRequest (URL, 'GET', processRepoListResponse);
-  });
+document.getElementById ('repoList').addEventListener ('click', function () {
+  // git hub url to get btford details
+  const userId = document.getElementById ('userId').value;
+  const URL = 'https://api.github.com/users/' + userId + '/repos';
+  makeRequest (URL, 'GET', repoListResponse);
+});
 
-  function processUserDetailsResponse (userData) {
-    console.log ('render user details', userData);
-  }
+function userDetailsResponse (userData) {
+  console.log ('render user details', userData);
+}
 
-  function processRepoListResponse (repoList) {
-    console.log ('render repo list', repoList);
-  }
-
+function repoListResponse (repoList) {
+  console.log ('render repo list', repoList);
+}
