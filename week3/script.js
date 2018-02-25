62bd504b2e3731dcec1e8637549f44cd24a583d4
Kemirdin <qemirdin@gmail.com>
2018-02-25 21:52:49 +0100

    update file

--- week3/script.js
+++ week3/script.js
@@ -1,54 +1,52 @@
 'use strict';
 // Create the XHR object.
 
-  function makeMyXHR(url, methodType) {
-    const promiseObj = new Promise(function (resolve, reject) {
-      const xhr = new XMLHttpRequest();
-      xhr.open(methodType, url, true);
-      xhr.send();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            console.log('xhr done successfully');
-            const resp = xhr.responseText;
-            const respJson = JSON.parse(resp);
-            resolve(respJson);
-          } else {
-            reject(xhr.status);
-            console.log('xhr failed');
-          }
+function makeMyXHR (url, methodType) {
+  const promiseObj = new Promise (function (resolve, reject) {
+    const xhr = new XMLHttpRequest ();
+    xhr.open (methodType, url, true);
+    xhr.send ();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log ('xhr done successfully');
+          const resp = xhr.responseText;
+          const respJson = JSON.parse (resp);
+          resolve (respJson);
         } else {
-          console.log('xhr processing going on');
+          reject (xhr.status);
+          console.log ('xhr failed');
         }
-      };
-      console.log('request sent succesfully');
-    });
-
-    return promiseObj;
-  }
-
-  document.getElementById('userDetails').addEventListener('click', function () {
-    
-    const userId = document.getElementById('userId').value;
-    const URL = 'https://api.github.com/users/' + userId;
-    makeMyXHR(URL, 'GET').then(uerDetailsResponse, errorHandler);
+      } else {
+        console.log ('xhr processing going on');
+      }
+    };
+    console.log ('request sent succesfully');
   });
 
-  document.getElementById('repoList').addEventListener('click', function () {
-    
-    const userId = document.getElementById('userId').value;
-    const URL = 'https://api.github.com/users/' + userId + '/repos';
-    makeMyXHR(URL, 'GET').then(repoListResponse, errorHandler);
-  });
+  return promiseObj;
+}
 
-  function uerDetailsResponse(userData) {
-    console.log('render user details', userData);
-  }
+document.getElementById ('userDetails').addEventListener ('click', function () {
+  const userId = document.getElementById ('userId').value;
+  const URL = 'https://api.github.com/users/' + userId;
+  makeMyXHR (URL, 'GET').then (uerDetailsResponse, errorHandler);
+});
 
-  function repoListResponse(repoList) {
-    console.log('render repo list', repoList);
-  }
+document.getElementById ('repoList').addEventListener ('click', function () {
+  const userId = document.getElementById ('userId').value;
+  const URL = 'https://api.github.com/users/' + userId + '/repos';
+  makeMyXHR (URL, 'GET').then (repoListResponse, errorHandler);
+});
 
-  function errorHandler(statusCode) {
-    console.log('failed with status', status);
-  }
+function uerDetailsResponse (userData) {
+  console.log ('render user details', userData);
+}
+
+function repoListResponse (repoList) {
+  console.log ('render repo list', repoList);
+}
+
+function errorHandler (statusCode) {
+  console.log ('failed with status', status);
+}
