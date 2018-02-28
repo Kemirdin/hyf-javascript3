a70d2d796036bcbf9a3bd1d2d10bb9a7f6bd3cec
Kemirdin <qemirdin@gmail.com>
2018-02-28 14:04:16 +0100

    update file

--- week3/script2.js
+++ week3/script2.js
@@ -1,53 +1,159 @@
 'use strict';
-// Create the XHR object.
 
-  function makeMyXHR(url, methodType) {
-    const promiseObj = new Promise(function (resolve, reject) {
-      const xhr = new XMLHttpRequest();
-      xhr.open(methodType, url, true);
-      xhr.send();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            console.log('xhr done successfully');
-            const resp = xhr.responseText;
-            const respJson = JSON.parse(resp);
-            resolve(respJson);
-          } else {
-            reject(xhr.status);
-            console.log('xhr failed');
-          }
-        } else {
-          console.log('xhr processing going on');
-        }
-      };
-      console.log('request sent successfully');
+{
+  const githubSearch = document.getElementById ('input');
+
+  const githubUsers = document.getElementById ('githubUsers');
+  const githubRep = document.getElementById ('githubRep');
+
+  document
+    .getElementById ('userDetails')
+    .addEventListener ('click', uerDetailsResponse, true);
+
+  document
+    .getElementById ('hyfList')
+    .addEventListener ('click', hyfListResponse, true);
+
+  function fetchJSON (url) {
+    // Return a new promise.
+
+    return new Promise ((resolve, reject) => {
+      // Do the usual XHR stuff
+      const req = new XMLHttpRequest ();
+      req.open ('GET', url);
+      req.responseType = 'json';
+
+      req.onload = () =>
+        // Resolve the promise with the response text
+        resolve (req.response);
+
+      // Otherwise reject with the status text
+      // which will hopefully be a meaningful error
+      // Handle network errors
+      req.onerror = () => reject (req.statusText);
+
+      // Make the request
+      req.send ();
     });
+  }
+  function hyfListResponse () {
+    fetchJSON ("https://api.github.com/repos/HackYourFuture/" + githubSearch.value).then (data => {
+      if (data.message) {
+        githubUsers.innerHTML = '';
+        githubRep.innerHTML = '';
+        createAndAppend ('h1', githubRep, 'this repository does not exist ');
+      } else {
+        githubRep.innerHTML = '';
+        githubUsers.innerHTML = '';
 
-    return promiseObj;
+        const repLink = data.svn_url;
+
+        const contributorsUrl = data.contributors_url;
+        const hyfRepLink =
+          '<a href="' + repLink + '">' + 'View ' + data.name + '</a>';
+
+        createAndAppend ('h1', githubRep, 'Repository Name : ' + data.name);
+        createAndAppend ('h2', githubRep, 'Description : ' + data.description);
+        createAndAppend ('h3', githubRep, ' Created at : ' + data.created_at);
+        createAndAppend ('h3', githubRep, ' Updated at ' + data.updated_at);
+        createAndAppend ('h3', githubRep, 'Pushed at : ' + data.pushed_at);
+        createAndAppend ('h3', githubRep, ' Forks : ' + data.forks);
+        createAndAppend ('h3', githubRep, 'On Branch : ' + data.default_branch);
+        createAndAppend ('h3', githubRep, hyfRepLink);
+
+        fetchJSON (contributorsUrl).then (data => {
+          createAndAppend ('h2', githubRep, 'Contributors : ');
+
+          data.forEach (element => {
+            const contributorImg = element.avatar_url;
+            const contributorImgLink = '<img src="' + contributorImg + '">';
+
+            createAndAppend ('h2', githubRep, ' - Name : ' + element.login);
+            createAndAppend ('div', githubRep, contributorImgLink);
+            createAndAppend (
+              'h2',
+              githubRep,
+              'contributions : ' + element.contributions
+            );
+          });
+        });
+      }
+    });
   }
 
-  document.getElementById('userDetails').addEventListener('click', function () {
-    const userId = document.getElementById('userId').value;
-    const url = 'https://api.github.com/users/' + userId;
-    makeMyXHR(url, 'GET').then(uerDetailsResponse, errorHandler);
-  });
+  function uerDetailsResponse () {
+    fetchJSON (
+      'https://api.github.com/users/' + githubSearch.value + '/repos'
+    ).then (data => {
+      if (data.message) {
+        githubUsers.innerHTML = '';
+        githubRep.innerHTML = '';
+        createAndAppend ('h1', githubUsers, 'this user does not exist');
+      } else {
+        githubUsers.innerHTML = '';
+        githubRep.innerHTML = '';
 
-  document.getElementById('repoList').addEventListener('click', function () {
-    const userId = document.getElementById('userId').value;
-    const url = 'https://api.github.com/users/' + userId + '/repos';
-    makeMyXHR(url, 'GET').then(repoListResponse, errorHandler);
-  });
+        const userName = document.createElement ('h1');
+        userName.innerHTML = data[0].owner.login;
+        githubUsers.appendChild (userName);
 
-  function uerDetailsResponse(userData) {
-    console.log('render user details', userData);
+        const userImg = document.createElement ('img');
+        userImg.setAttribute ('src', data[0].owner.avatar_url);
+        githubUsers.appendChild (userImg);
+
+        const userLink = document.createElement ('a');
+        userLink.setAttribute ('href', data[0].owner.html_url);
+        userLink.innerHTML = '<h3>' + 'Visit user' + '</h3>';
+
+        githubUsers.appendChild (userLink);
+        // createAndAppend('h3', users, '<a href="' + data[0].owner.html_url + '">' + 'visit user' + '</a>');
+        createAndAppend ('h1', githubUsers, 'Repositories :');
+
+        for (const key in data) {
+          const repositoryDiv = document.createElement ('div');
+          repositoryDiv.setAttribute ('id', data[key].name);
+
+          const nameOfRep = document.createElement ('h2');
+          const description = document.createElement ('h3');
+          const created = document.createElement ('h3');
+          const updated = document.createElement ('h3');
+          createAndAppend ('h2', githubUsers, '<br>' + '<~~~~~~~~~~~~~~~~>');
+          nameOfRep.innerHTML = '-Repository name : ' + data[key].name;
+          description.innerHTML = 'Description : ' + data[key].description;
+          created.innerHTML = 'Created at : ' + data[key].created_at;
+          updated.innerHTML = 'Updated at : ' + data[key].updated_at;
+
+          githubUsers.appendChild (repositoryDiv);
+          repositoryDiv.appendChild (nameOfRep);
+          repositoryDiv.appendChild (description);
+          repositoryDiv.appendChild (created);
+          repositoryDiv.appendChild (updated);
+
+          createAndAppend ('h1', repositoryDiv, '<br>' + 'Contributors : ');
+
+          fetchJSON (data[key].contributors_url).then (contData => {
+            for (let i = 0; i < contData.length; i++) {
+              const ContributorName = document.createElement ('h2');
+              const ContributorPic = document.createElement ('img');
+
+              ContributorPic.setAttribute ('src', contData[i].avatar_url);
+              ContributorName.innerHTML = contData[i].login;
+
+              repositoryDiv.appendChild (ContributorName);
+              repositoryDiv.appendChild (ContributorPic);
+            }
+          });
+        }
+      }
+    });
   }
 
-  function repoListResponse(repoList) {
-    console.log('render repo list', repoList);
-  }
-
-  function errorHandler(statusCode) {
-    console.log('failed with status', status);
+  function createAndAppend (name, parent, innerHTML) {
+    const child = document.createElement (name);
+    parent.appendChild (child);
+    if (innerHTML !== undefined) {
+      child.innerHTML = innerHTML;
+    }
+    return child;
   }
 }
